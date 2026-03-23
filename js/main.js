6fe944af318dcd4f7fe9f52e693b44a8dd2fdca7
penach0 <josemanuel.penacho@gmail.com>
2026-03-23 23:15:08 +0000

    Add basic lighting effects

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+scene.add(ambientLight);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 1.5);
+sunLight.position.set(3, 5, 3);
+scene.add(sunLight);
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
@@ -13,13 +20,14 @@ document.body.appendChild(renderer.domElement);
 
 // Sphere
 const geometry = new THREE.SphereGeometry(1, 64, 64);
-const material = new THREE.MeshBasicMaterial({ color: 0x4444ff });
+const material = new THREE.MeshStandardMaterial({ color: 0x44aaff });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
 // Animation
 function animate() {
-	sphere.rotation.y += 0.005;
+	sunLight.position.x = Math.sin(Date.now() * 0.001) * 5;
+	sunLight.position.z = Math.cos(Date.now() * 0.001) * 5;
 	renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
