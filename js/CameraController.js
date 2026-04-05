881f76b5982a9060686516083cd42ece2a29963b
penach0 <josemanuel.penacho@gmail.com>
2026-04-05 23:01:38 +0100

    Improve player camera self-centering
    - Centering implied always moving for the necessary amount for the yaw
      and pitch to come back to the original position. Now a single tap to
      move can trigger a full centering

--- js/CameraController.js
+++ js/CameraController.js
@@ -1,13 +1,14 @@
 class CameraController {
-	constructor(target, isAutoCenter = true) {
+	constructor(target, autoCenter = true) {
 		this.target = target;
-		this._isAutoCenter = isAutoCenter;
+		this.autoCenterEnabled = autoCenter;
 
 		this.yaw = 0;
 		this.pitch = -0.6;
 		this.sensitivity = 0.001;
 
 		this.isDragging = false;
+		this.isCentering = false;
 	}
 
 	onMouseDown(e) {
@@ -21,6 +22,8 @@ class CameraController {
 	onMouseMove(e) {
 		if (!this.isDragging) return;
 
+		this.isCentering = false;
+
 		this.yaw -= e.movementX * this.sensitivity;
 		this.pitch -= e.movementY * this.sensitivity;
 
@@ -30,7 +33,7 @@ class CameraController {
 	update() {
 		const pivot = this.target.cameraPivot;
 
-		if (this._isAutoCenter) this._autoCenter();
+		if (this.autoCenterEnabled) this._autoCenter();
 
 		// Smooth movement
 		pivot.rotation.y += (this.yaw - pivot.rotation.y) * 0.1;
@@ -39,12 +42,24 @@ class CameraController {
 
 	_autoCenter() {
 		if (!this.isDragging && this.target.isMoving) {
+			this.isCentering = true;
+		}
+
+		if (this.isCentering) {
 			const targetYaw = 0;
 			const targetPitch = -0.4;
 
 			// Control speed of follow
 			this.yaw += (targetYaw - this.yaw) * 0.03;
 			this.pitch += (targetPitch - this.pitch) * 0.03;
+
+			// Stop centering if centered
+			if (Math.abs(this.yaw - targetYaw) < 0.001 &&
+				Math.abs(this.pitch - targetPitch) < 0.001) {
+				this.yaw = targetYaw;
+				this.pitch = targetPitch;
+				this.isCentering = false;
+			}
 		}
 	}
 }
