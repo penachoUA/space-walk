c194e6fccadb571b158617b62f3f04f64e04d4ff
penach0 <josemanuel.penacho@gmail.com>
2026-04-09 03:44:55 +0100

    Add Skybox

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
+import * as THREE from 'three';
 import { renderer, scene } from './core/scene.js';
 import Game from './core/Game.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-const game = new Game(scene, renderer, true);
+new RGBELoader().load('../assets/skybox.hdr', (texture) => {
+	texture.mapping = THREE.EquirectangularReflectionMapping;
+	scene.background = texture;
+	new Game(scene, renderer);
+});
